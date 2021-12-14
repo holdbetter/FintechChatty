5eaafd1d5f3865f2346509e21d76f71c783197ad
Vilen Evseev <holdbetter1@gmail.com>
2021-12-14 23:40:44 +0300

    empty topic state removed

--- app/src/main/java/com/holdbetter/fintechchatproject/app/bottomnavigation/navigation/channels/view/TopicAdapter.kt
+++ app/src/main/java/com/holdbetter/fintechchatproject/app/bottomnavigation/navigation/channels/view/TopicAdapter.kt
@@ -3,69 +3,37 @@ package com.holdbetter.fintechchatproject.app.bottomnavigation.navigation.channe
 import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.holdbetter.fintechchatproject.R
 import com.holdbetter.fintechchatproject.databinding.TopicInstanceBinding
 import com.holdbetter.fintechchatproject.model.Topic
 
 class TopicAdapter(private val onTopicClicked: (Context, Topic) -> Unit) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var wasSomethingSubmitted: Boolean = false
-    private val emptyListItemCount = 1
+    RecyclerView.Adapter<TopicAdapter.TopicViewHolder>() {
     private val asyncDiffer = AsyncListDiffer(this, TopicDiffUtilCallback())
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopicViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        when (viewType) {
-            TopicViewType.TOPIC_INSTANCE.ordinal -> return TopicViewHolder(
-                TopicInstanceBinding.inflate(
-                    inflater,
-                    parent,
-                    false
-                ), onTopicClicked
-            )
-            TopicViewType.NO_TOPICS_IN_THIS_STREAM.ordinal -> return EmptyViewHolder(
-                inflater.inflate(
-                    R.layout.no_topic_in_stream_instance,
-                    parent,
-                    false
-                )
-            )
-        }
-        throw Exception("No corresponding viewType for $viewType value")
+        return TopicViewHolder(
+            TopicInstanceBinding.inflate(
+                inflater,
+                parent,
+                false
+            ), onTopicClicked
+        )
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is TopicViewHolder -> holder.bind(asyncDiffer.currentList[position])
-        }
+
+    override fun onBindViewHolder(holder: TopicViewHolder, position: Int) {
+        holder.bind(asyncDiffer.currentList[position])
     }
 
-    override fun getItemCount() = when {
-        asyncDiffer.currentList.size != 0 -> {
-            asyncDiffer.currentList.size
-        }
-        wasSomethingSubmitted -> {
-            emptyListItemCount
-        }
-        else -> {
-            0
-        }
-    }
+    override fun getItemCount() = asyncDiffer.currentList.size
 
     fun submitList(topic: List<Topic>) {
-        wasSomethingSubmitted = true
         asyncDiffer.submitList(topic)
-    }
-
-    override fun getItemViewType(position: Int) = if (asyncDiffer.currentList.size == 0) {
-        TopicViewType.NO_TOPICS_IN_THIS_STREAM.ordinal
-    } else {
-        TopicViewType.TOPIC_INSTANCE.ordinal
     }
 
     class TopicViewHolder(
@@ -82,13 +50,6 @@ class TopicAdapter(private val onTopicClicked: (Context, Topic) -> Unit) :
                 }
             }
         }
-    }
-
-    class EmptyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
-    enum class TopicViewType {
-        NO_TOPICS_IN_THIS_STREAM,
-        TOPIC_INSTANCE
     }
 
     class TopicDiffUtilCallback : DiffUtil.ItemCallback<Topic>() {
